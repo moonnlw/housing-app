49633bfa50c8ae0147fd2a8aa0e6a25428726f45
sadovnichek <kapkaevictor@gmail.com>
2022-12-14 11:55:55 +0500

    hide database credentials

--- app/src/main/java/com/example/houseapp/utils/DatabaseConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.example.houseapp.utils
-
-object DatabaseConfig {
-    const val jdbcUrl = "jdbc:postgresql://ec2-3-216-167-65.compute-1.amazonaws.com:5432/d5414vl6ij5i2f?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
-    const val driver = "org.postgresql.Driver"
-    const val user = "avinugmjzprnkv"
-    const val password = "525799763887e66a60857bb4b059e013cc650bb9dbf86c28077ed7235a7ca159"
-}
