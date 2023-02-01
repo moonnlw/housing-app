688449cfb079bdf4cb41a537f55961a68274ec4f
dudeunderthemoonlight <l.a.v.192002@gmail.com>
2023-02-01 16:52:33 +0500

    2 way databinding impl

--- app/src/main/java/com/example/houseapp/data/models/User.kt
+++ app/src/main/java/com/example/houseapp/data/models/User.kt
@@ -1,9 +1,43 @@
 package com.example.houseapp.data.models
 
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+import com.example.houseapp.BR
+
 data class User(
-    val userId: String,
-    val firstName: String?,
-    val lastName: String?,
-    val address: String?,
-    val phone: String?
-)
+    val id: String,
+    var firstName: String = "",
+    var lastName: String = "",
+    var address: String = "",
+    var phone: String = ""
+) : BaseObservable() {
+
+    var bFirstName: String
+        @Bindable get() = firstName
+        set(value) {
+            firstName = value
+            notifyPropertyChanged(BR.bFirstName)
+        }
+
+    var bLastName: String
+        @Bindable get() = lastName
+        set(value) {
+            lastName = value
+            notifyPropertyChanged(BR.bLastName)
+        }
+
+    var bAddress: String
+        @Bindable get() = address
+        set(value) {
+            address = value
+            notifyPropertyChanged(BR.bAddress)
+        }
+
+    var bPhone: String
+        @Bindable get() = phone
+        set(value) {
+            phone = value
+            notifyPropertyChanged(BR.bPhone)
+        }
+}
+
