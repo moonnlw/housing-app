e115041041cdf6de565a45e3f211e5b83aa3fce7
dudeunderthemoonlight <l.a.v.192002@gmail.com>
2023-02-05 17:57:00 +0500

    delete bottom navigation view for AdminActivity.kt

--- app/src/main/java/com/example/houseapp/AdminActivity.kt
+++ app/src/main/java/com/example/houseapp/AdminActivity.kt
@@ -3,30 +3,58 @@ package com.example.houseapp
 import android.content.Context
 import android.graphics.Rect
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.houseapp.databinding.ActivityMainAdminBinding
 
-class AdminActivity: AppCompatActivity() {
+/**
+ * Главное активити админа, отрисовывает UI для админа
+ */
+class AdminActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    private val authViewModel: AuthViewModel by viewModels {
+        (application as MyApplication).appContainer.viewModelFactory
+    }
 
-        setContentView(R.layout.activity_main_admin)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initializeUI()
+        observeAuthorizationStatus()
+    }
+
+    private fun initializeUI() {
+        val binding = ActivityMainAdminBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_container_admin) as NavHostFragment
         navController = navHostFragment.navController
 
+        setSupportActionBar(binding.toolbar)
+
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.usersList))
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    /**
+     * В случае окончания сессии -> навигация к [AuthActivity]
+     */
+    private fun observeAuthorizationStatus() {
+        authViewModel.userIdLiveData.observe(this) {
+            if (it.isNullOrEmpty()) navController.navigate(AuthActivityDirections.navigateToAuthActivity())
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
