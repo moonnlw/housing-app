8f3307cf1fc31e44938e3754fd0ea1ad1a4e9794
dudeunderthemoonlight <l.a.v.192002@gmail.com>
2023-02-05 18:09:27 +0500

    наследование от BaseFragment

--- app/src/main/java/com/example/houseapp/ui/user/newrequestscreen/CreateRequestFragment.kt
+++ app/src/main/java/com/example/houseapp/ui/user/newrequestscreen/CreateRequestFragment.kt
@@ -1,60 +1,27 @@
 package com.example.houseapp.ui.user.newrequestscreen
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
-import com.example.houseapp.MyApplication
+import androidx.navigation.fragment.navArgs
 import com.example.houseapp.R
 import com.example.houseapp.databinding.FragmentCreateRequestBinding
+import com.example.houseapp.ui.BaseFragment
 
 /**
  * Форма создания новой заявки
  */
-class CreateRequestFragment : Fragment() {
+class CreateRequestFragment :
+    BaseFragment<FragmentCreateRequestBinding, CreateRequestViewModel>
+        (R.layout.fragment_create_request) {
 
-    private val createRequestViewModel: CreateRequestViewModel by activityViewModels {
-        (requireActivity().application as MyApplication).appContainer.viewModelFactory
+    private val args by navArgs<CreateRequestFragmentArgs>()
+
+    override val fragmentViewModel:
+            CreateRequestViewModel by activityViewModels { appContainer.viewModelFactory }
+
+    override fun CreateRequestViewModel.initialize() {
+        userId.value = args.userId
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val binding = DataBindingUtil.inflate<FragmentCreateRequestBinding?>(
-            inflater,
-            R.layout.fragment_create_request,
-            container,
-            false
-        ).apply {
-            lifecycleOwner = viewLifecycleOwner
-            viewModel = createRequestViewModel
-        }
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        observeData()
-    }
-
-    private fun observeData() {
-        createRequestViewModel.isEnoughData.observe(viewLifecycleOwner) { event ->
-            event.getContentIfNotHandled()?.let { isEnough ->
-                if (!isEnough) {
-                    findNavController().navigate(R.id.action_navigate_home)
-                }
-            }
-        }
-        createRequestViewModel.message.observe(viewLifecycleOwner) { event ->
-            event.getContentIfNotHandled()?.let {
-                Toast.makeText(activity, it, Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
+    override fun onReady(savedInstanceState: Bundle?) { }
 }
