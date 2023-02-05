be4ceabf27de4fe771eb7eef9e477336d7ebb88e
dudeunderthemoonlight <l.a.v.192002@gmail.com>
2023-02-05 18:16:26 +0500

    2-way data-binding with BaseObservable field

--- app/src/main/java/com/example/houseapp/data/models/UserRequest.kt
+++ app/src/main/java/com/example/houseapp/data/models/UserRequest.kt
@@ -1,11 +1,23 @@
 package com.example.houseapp.data.models
 
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+import com.example.houseapp.BR
+
 data class UserRequest(
     val requestId: Int? = null,
     val userId: String,
     val problemType: String,
     val description: String,
     val isDone: Boolean = false,
-    var answer: String? = null,
-    var solution: Boolean? = null
-)
+    var answer: String = "",
+    var solution: Boolean = false
+) : BaseObservable() {
+
+    var bAnswer: String
+        @Bindable get() = answer
+        set(value) {
+            answer = value
+            notifyPropertyChanged(BR.bAnswer)
+        }
+}
