aac4b56948c4d076c04bc65c69ddb3952f8d355b
dudeunderthemoonlight <l.a.v.192002@gmail.com>
2023-02-05 18:00:41 +0500

    перенес проверку на авторизацию во фрагмент LoginFragment.kt

--- app/src/main/java/com/example/houseapp/AuthViewModel.kt
+++ app/src/main/java/com/example/houseapp/AuthViewModel.kt
@@ -1,41 +1,14 @@
 package com.example.houseapp
 
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
 import com.example.houseapp.data.repos.AuthRepository
-import com.example.houseapp.data.repos.UserRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-
+import com.example.houseapp.ui.BaseViewModel
 
 class AuthViewModel(
-    authRepository: AuthRepository,
-    private val userRepository: UserRepository
-) : ViewModel() {
+    authRepository: AuthRepository
+) : BaseViewModel() {
 
-    private val userId = authRepository.getUserId()
-    private val _isAuthorized = MutableLiveData<Boolean>()
-
-    val isAuthorized: LiveData<Boolean> get() = _isAuthorized
-    var isAdmin: Boolean = false
-
-    /**
-     * Проверка на авторизацию и на роль пользователя (админ / юзер).
-     */
-    init {
-        viewModelScope.launch(Dispatchers.Default) {
-            val authorized = isUserAuthorized(userId)
-            if (authorized) {
-                isAdmin = userRepository.isUserAdmin(userId!!)
-            }
-            _isAuthorized.postValue(authorized)
-        }
-    }
-
-    /**
-     * Проверяет авторизован ли пользователь && есть ли запись о пользователе в Room.
-     * Запись в Room отсутствует только в случае, если вся база пуста.
-     */
-    private suspend fun isUserAuthorized(id: String?): Boolean =
-        id != null
-                && !userRepository.isUserDatabaseEmpty()
+    val userId: String? = authRepository.getUserId()
+    val userIdLiveData: LiveData<String?> = authRepository.observeUserId().asLiveData()
 }
--- app/src/main/java/com/example/houseapp/ui/user/loginscreen/LoginViewModel.kt
+++ app/src/main/java/com/example/houseapp/ui/user/loginscreen/LoginViewModel.kt
@@ -1,47 +1,75 @@
 package com.example.houseapp.ui.user.loginscreen
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.example.houseapp.data.repos.AuthRepository
 import com.example.houseapp.data.repos.UserRepository
-import com.example.houseapp.data.models.Event
 import com.example.houseapp.data.models.Response.Success
 import com.example.houseapp.data.models.Response.Failure
+import com.example.houseapp.ui.BaseViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.launch
 
-
+@OptIn(ExperimentalCoroutinesApi::class)
 class LoginViewModel(
     private val authRepository: AuthRepository,
     private val userRepository: UserRepository
-) : ViewModel() {
+) : BaseViewModel() {
 
-    private val statusMessage = MutableLiveData<Event<String>>()
-    private val _isLoading = MutableLiveData<Boolean>()
-    private val _isAuthorized = MutableLiveData<Boolean>()
+    private val userIdFlow: Flow<String?> = authRepository.observeUserId()
 
-    val message: LiveData<Event<String>> get() = statusMessage
-    val isLoading: LiveData<Boolean> get() = _isLoading
-    val isAuthorized: LiveData<Boolean> get() = _isAuthorized
+    val userId = authRepository.getUserId()
 
+    /**
+     * Пользователь авторизован если [authRepository].userId станет не равен null && появятся записи в Room
+     */
+    private val isAuthorized: Flow<Boolean> = userIdFlow.mapLatest {
+        it != null && !userRepository.isUserDatabaseEmpty()
+    }
+
+    /**
+     * Поля для 2-way data-binding
+     */
     val userEmail = MutableLiveData("")
     val userPassword = MutableLiveData("")
 
+    /**
+     * Инициализация: проверка на авторизацию пользователя
+     * Если первое значение испускаемое isAuthorized : Flow<Boolean> == true значит пользователь авторизован
+     */
+    init {
+        _isLoading.value = true
+
+        viewModelScope.launch {
+            if (isAuthorized.first()) {
+                finishAuth()
+            } else {
+                _isLoading.postValue(false)
+            }
+        }
+    }
+
+    /**
+     * Срабатывает при нажатии на кнопку login (data-binding)
+     * Валидация опреации логина
+     */
     fun loginUser() {
         val email = userEmail.value
         val password = userPassword.value
 
         if (email.isNullOrBlank() || password.isNullOrBlank()) {
-            statusMessage.value = Event("Please fill all the fields")
+            showMessage("Please fill all the fields")
         } else {
             _isLoading.value = true
+
             viewModelScope.launch {
                 when (val result = authRepository.signIn(email, password)) {
-                    is Success -> refreshUserInfo(result.data.id)
+                    is Success -> loadData()
                     is Failure -> {
-                        statusMessage.postValue(Event("Error occurred, please, try again later"))
+                        showMessage("Error occurred, please, try again later")
                         Log.e(javaClass.simpleName, result.ex.message.toString())
                         _isLoading.postValue(false)
                     }
@@ -50,14 +78,65 @@ class LoginViewModel(
         }
     }
 
+    /**
+     * Подгружает данные о пользователе/пользователях из БД
+     */
+    private suspend fun loadData() {
+        val userId = authRepository.getUserId()!!
+        if (userRepository.isUserAdmin(userId)) refreshAllUsersInfo() else refreshUserInfo(userId)
+    }
+
+    /**
+     * Обновление данных одного пользователя и обработка запроса
+     */
     private suspend fun refreshUserInfo(id: String) {
+        Log.e(this.javaClass.simpleName, "refreshing info")
         when (val result = userRepository.refreshUser(id)) {
-            is Success -> _isAuthorized.postValue(true)
+            is Success -> {
+                Log.i(this.javaClass.simpleName, "User info is refreshed")
+                finishAuth()
+            }
             is Failure -> {
-                statusMessage.postValue(Event("Error occurred, please, try again later"))
+                showMessage("Error occurred, please, try again later")
                 Log.e(this.javaClass.simpleName, result.ex.message.toString())
-                _isLoading.postValue(false)
             }
         }
+    }
+
+    /**
+     * Обновление данных всех пользователя и обработка запроса (для админа)
+     */
+    private suspend fun refreshAllUsersInfo() {
+        when (val result = userRepository.refreshAllUsers()) {
+            is Success -> {
+                Log.i(this.javaClass.simpleName, "All users are refreshed")
+                finishAuth()
+            }
+            is Failure -> {
+                showMessage("Error occurred, please, try again later")
+                Log.e(this.javaClass.simpleName, result.ex.message.toString())
+            }
+        }
+    }
+
+    /**
+     * Заканчивает авторизацию, проверет роль пользователя и производит навигацию в соответсвующее
+     * Activity
+     */
+    private fun finishAuth() {
+        viewModelScope.launch {
+            val userId = authRepository.getUserId()!!
+
+            if (userRepository.isUserAdmin(userId)) {
+                navigate(LoginFragmentDirections.navigateToAdminActivity())
+            } else {
+                Log.e(this.javaClass.simpleName, "Auth is finished")
+                navigate(LoginFragmentDirections.navigateToUserActivity())
+            }
+        }
+    }
+
+    fun navigateToSignup() {
+        navigate(LoginFragmentDirections.navigateToSignupFragment())
     }
 }
